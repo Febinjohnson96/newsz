1cc5695bf76d8a56ee876803a4dc661e5113bea2
Febin Johnson <febinnj1996@gmail.com>
2023-02-25 00:45:54 +0530

    added all onboarding titles

--- lib/src/presentation/onboarding/widgets/onboarding_titles.dart
+++ lib/src/presentation/onboarding/widgets/onboarding_titles.dart
@@ -14,13 +14,11 @@ class OnboardingTitles extends StatelessWidget {
       controller: pageController,
       children: [
         RichText(
-            text: TextSpan(
-                text: "Get the latest news from",
-                style: Theme.of(context).textTheme.headline2?.copyWith(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    color: Colors.white),
-                children: [
+          text: TextSpan(
+            text: "Get the latest news from",
+            style: Theme.of(context).textTheme.headline2?.copyWith(
+                fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),
+            children: [
               TextSpan(
                 text: "\nreliable\nsources",
                 style: Theme.of(context).textTheme.headline2?.copyWith(
@@ -37,7 +35,59 @@ class OnboardingTitles extends StatelessWidget {
                       fontSize: 35,
                     ),
               )
-            ]))
+            ],
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            text: "Still",
+            style: Theme.of(context).textTheme.headline2?.copyWith(
+                fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),
+            children: [
+              TextSpan(
+                text: "up to date",
+                style: Theme.of(context).textTheme.headline2?.copyWith(
+                      color: redRibbon,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+              ),
+              TextSpan(
+                text: "news from all around the world",
+                style: Theme.of(context).textTheme.headline2?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+              )
+            ],
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            text: "From art to politics,",
+            style: Theme.of(context).textTheme.headline2?.copyWith(
+                fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),
+            children: [
+              TextSpan(
+                text: "anything",
+                style: Theme.of(context).textTheme.headline2?.copyWith(
+                      color: redRibbon,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+              ),
+              TextSpan(
+                text: "in newz",
+                style: Theme.of(context).textTheme.headline2?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+              )
+            ],
+          ),
+        ),
       ],
     );
   }

21c07bc8ae09ccb55aec81e09ce1fc8bb9644165
Febin Johnson <febinnj1996@gmail.com>
2023-03-15 18:23:57 +0530

    Completed sign ui

--- lib/src/presentation/signin/signin_screen.dart
+++ lib/src/presentation/signin/signin_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:newsz/src/config/theme/appcolors.dart';
 import 'package:newsz/src/presentation/signin/signin_controller.dart';
+import 'package:newsz/src/widgets/appbutton_with_icon.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -25,7 +27,7 @@ class SignInScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(40),
                             child: Image.asset(
                               "assets/images/signup/parliament.jpg",
-                              height: constraints.maxHeight * 0.6,
+                              height: constraints.maxHeight * 0.5,
                               width: constraints.maxWidth,
                               fit: BoxFit.cover,
                             ),
@@ -35,7 +37,7 @@ class SignInScreen extends StatelessWidget {
                           padding: const EdgeInsets.symmetric(
                               horizontal: 16, vertical: 10),
                           child: Container(
-                            height: constraints.maxHeight * 0.6,
+                            height: constraints.maxHeight * 0.5,
                             width: constraints.maxWidth,
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(40),
@@ -44,15 +46,79 @@ class SignInScreen extends StatelessWidget {
                           ),
                         ),
                         Positioned(
-                          bottom: constraints.maxHeight*0.08,
-                          left: constraints.maxWidth*0.1,
+                          bottom: constraints.maxHeight * 0.08,
+                          left: constraints.maxWidth * 0.1,
                           child: Text(
                             'Sign in\noptions.',
-                            style: Theme.of(context).textTheme.headline1?.copyWith(color: Colors.white,fontSize: 32),
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1
+                                ?.copyWith(color: Colors.white, fontSize: 32),
                           ),
                         )
                       ],
-                    )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: SizedBox(
+                        height: constraints.maxHeight * 0.08,
+                        child: const AppButtonWithIcon(
+                          btntitle: 'Continue with Email',
+                          btnColor: redRibbon,
+                          iconImagePath: "assets/images/icons/mail1.png",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: SizedBox(
+                        height: constraints.maxHeight * 0.08,
+                        child: const AppButtonWithIcon(
+                          btntitle: 'Sign In with Google',
+                          btnColor: athensGrey,
+                          iconImagePath: "assets/images/icons/google1.png",
+                          textColor: blueVogue,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 11, horizontal: 20),
+                      child: SizedBox(
+                        height: constraints.maxHeight * 0.08,
+                        child: const AppButtonWithIcon(
+                          btntitle: 'Sign In with Apple ID',
+                          btnColor: athensGrey,
+                          iconImagePath:
+                              "assets/images/icons/appleLogoBlack1.png",
+                          textColor: blueVogue,
+                        ),
+                      ),
+                    ),
+                    RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: "Don't have an account?",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(
+                                  color: lynch, fontWeight: FontWeight.w500)),
+                      TextSpan(
+                          text: "Sign Up",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(
+                                  color: redRibbon,
+                                  fontWeight: FontWeight.bold),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              debugPrint("TESTss");
+                            })
+                    ]))
                   ],
                 );
               }),
---
+++ lib/src/widgets/appbutton_with_icon.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppButtonWithIcon extends StatelessWidget {
+  const AppButtonWithIcon({
+    Key? key,
+    required this.btntitle,
+    required this.btnColor,
+    required this.iconImagePath,
+    this.callback,
+    this.textColor,
+  }) : super(key: key);
+  final String btntitle;
+  final Color btnColor;
+  final Color? textColor;
+  final String iconImagePath;
+  final VoidCallback? callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: callback,
+      child: Container(
+        decoration: BoxDecoration(
+            color: btnColor, borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                iconImagePath,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(width: 10,),
+              Text(
+                btntitle,
+                style: Theme.of(context).textTheme.headline6?.copyWith(
+                    color: textColor ?? Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

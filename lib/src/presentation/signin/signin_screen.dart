0ecdaf7f1e7751ef6d487b81fa9e0f3297568c4b
Febin Johnson <febinnj1996@gmail.com>
2023-03-17 15:37:32 +0530

    fixed screens for smaller devices

--- lib/src/presentation/signin/signin_screen.dart
+++ lib/src/presentation/signin/signin_screen.dart
@@ -21,110 +21,114 @@ class SignInScreen extends StatelessWidget {
                 return FadeInUp(
                   duration: const Duration(milliseconds: 1400),
                   delay: const Duration(milliseconds: 300),
-                  child: Column(
-                    children: [
-                      Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 10),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(40),
-                              child: Image.asset(
-                                parliament,
-                                height: constraints.maxHeight * 0.5,
-                                width: constraints.maxWidth,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 10),
-                            child: Container(
-                              height: constraints.maxHeight * 0.5,
-                              width: constraints.maxWidth,
-                              decoration: BoxDecoration(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 10),
+                              child: ClipRRect(
                                 borderRadius: BorderRadius.circular(40),
-                                color: blueVogue.withOpacity(0.5),
+                                child: Image.asset(
+                                  parliament,
+                                  height: constraints.maxHeight * 0.6,
+                                  width: constraints.maxWidth,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 10),
+                              child: Container(
+                                height: constraints.maxHeight * 0.6,
+                                width: constraints.maxWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: blueVogue.withOpacity(0.5),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: constraints.maxHeight * 0.08,
+                              left: constraints.maxWidth * 0.1,
+                              child: Text(
+                                'Sign in\noptions.',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline1
+                                    ?.copyWith(color: Colors.white, fontSize: 32),
+                              ),
+                            )
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: SizedBox(
+                            height: constraints.maxHeight * 0.08,
+                            child: const AppButtonWithIcon(
+                              btntitle: 'Continue with Email',
+                              btnColor: redRibbon,
+                              iconImagePath: mail,
+                            ),
                           ),
-                          Positioned(
-                            bottom: constraints.maxHeight * 0.08,
-                            left: constraints.maxWidth * 0.1,
-                            child: Text(
-                              'Sign in\noptions.',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: SizedBox(
+                            height: constraints.maxHeight * 0.08,
+                            child: const AppButtonWithIcon(
+                              btntitle: 'Sign In with Google',
+                              btnColor: athensGrey,
+                              iconImagePath: google,
+                              textColor: blueVogue,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 11, horizontal: 20),
+                          child: SizedBox(
+                            height: constraints.maxHeight * 0.08,
+                            child: const AppButtonWithIcon(
+                              btntitle: 'Sign In with Apple ID',
+                              btnColor: athensGrey,
+                              iconImagePath:
+                                  apple,
+                              textColor: blueVogue,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 5,),
+                        RichText(
+                            text: TextSpan(children: [
+                          TextSpan(
+                              text: "Don't have an account? ",
                               style: Theme.of(context)
                                   .textTheme
-                                  .headline1
-                                  ?.copyWith(color: Colors.white, fontSize: 32),
-                            ),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: SizedBox(
-                          height: constraints.maxHeight * 0.08,
-                          child: const AppButtonWithIcon(
-                            btntitle: 'Continue with Email',
-                            btnColor: redRibbon,
-                            iconImagePath: mail,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: SizedBox(
-                          height: constraints.maxHeight * 0.08,
-                          child: const AppButtonWithIcon(
-                            btntitle: 'Sign In with Google',
-                            btnColor: athensGrey,
-                            iconImagePath: google,
-                            textColor: blueVogue,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 11, horizontal: 20),
-                        child: SizedBox(
-                          height: constraints.maxHeight * 0.08,
-                          child: const AppButtonWithIcon(
-                            btntitle: 'Sign In with Apple ID',
-                            btnColor: athensGrey,
-                            iconImagePath:
-                                apple,
-                            textColor: blueVogue,
-                          ),
-                        ),
-                      ),
-                      RichText(
-                          text: TextSpan(children: [
-                        TextSpan(
-                            text: "Don't have an account? ",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5
-                                ?.copyWith(
-                                    color: lynch, fontWeight: FontWeight.w500)),
-                        TextSpan(
-                            text: "Sign Up",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5
-                                ?.copyWith(
-                                    color: redRibbon,
-                                    fontWeight: FontWeight.bold),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                debugPrint("TESTss");
-                              })
-                      ]))
-                    ],
+                                  .headline5
+                                  ?.copyWith(
+                                      color: lynch, fontWeight: FontWeight.w500)),
+                          TextSpan(
+                              text: "Sign Up",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5
+                                  ?.copyWith(
+                                      color: redRibbon,
+                                      fontWeight: FontWeight.bold),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  debugPrint("TESTss");
+                                })
+                        ])),
+                         const SizedBox(height: 5,),
+                      ],
+                    ),
                   ),
                 );
               }),

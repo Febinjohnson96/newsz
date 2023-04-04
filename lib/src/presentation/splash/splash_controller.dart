1efca2bc88342f1d5e8cf5694ad327becc84dceb
Febin Johnson <febinnj1996@gmail.com>
2023-04-04 18:10:52 +0530

    added hive to signup and route to home if has data

--- lib/src/presentation/splash/splash_controller.dart
+++ lib/src/presentation/splash/splash_controller.dart
@@ -1,15 +1,35 @@
 import 'package:get/get.dart';
+import 'package:get_it/get_it.dart';
+import 'package:newsz/src/data/data_sources/local/hive_provider.dart';
+import 'package:newsz/src/data/models/user_signup_model.dart';
 
 class SplashController extends GetxController {
+  final hive = GetIt.I.get<HiveProvider>();
+
+  final _currentAccount = Rxn<UserSignUpModel>();
+  UserSignUpModel? get currentAccount => _currentAccount.value;
+
+  UserSignUpModel? userdata;
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
-    reDirectToOnboard();
+    await fetchUserDataFromHive();
+    await reDirectToOnboard();
   }
 
-  void reDirectToOnboard() {
+  Future<void> reDirectToOnboard() async {
     Future.delayed(const Duration(milliseconds: 1400), () {
-      Get.offNamed("/onboarding");
+      if (currentAccount!.userName!.isEmpty) {
+        Get.offNamed("/onboarding");
+      } else {
+        Get.offNamed('/home');
+      }
     });
+  }
+
+  Future<void> fetchUserDataFromHive() async {
+    await hive.openUserBox();
+    final user = hive.readDataFromUserBox();
+    _currentAccount(user);
   }
 }

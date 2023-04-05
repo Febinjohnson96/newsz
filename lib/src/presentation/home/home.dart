8e672397466a2a4fc228d5dc8c1001f428bf79a8
Febin Johnson <febinnj1996@gmail.com>
2023-04-05 15:37:40 +0530

    Added hero news element to home page

--- lib/src/presentation/home/home.dart
+++ lib/src/presentation/home/home.dart
@@ -1,6 +1,9 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:newsz/src/config/theme/appcolors.dart';
 import 'package:newsz/src/presentation/home/home_controller.dart';
+import 'package:newsz/src/presentation/home/widgets/hero_news_element.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -11,10 +14,55 @@ class HomeView extends StatelessWidget {
         init: HomeController(),
         builder: (controller) {
           return Scaffold(
-            body: Column(
-              children: [],
-            ),
-          );
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+              body: Obx(
+                () => controller.isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          color: redRibbon,
+                        ),
+                      )
+                    : FadeInUp(
+                      child: LayoutBuilder(builder: (context, constraints) {
+                          return SingleChildScrollView(
+                            physics: const BouncingScrollPhysics(),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 15),
+                                  child: SizedBox(
+                                    height: constraints.maxHeight * 0.6,
+                                    child: ListView.separated(
+                                      separatorBuilder: (context, index) =>
+                                          const SizedBox(
+                                        width: 10,
+                                      ),
+                                      scrollDirection: Axis.horizontal,
+                                      physics: const BouncingScrollPhysics(),
+                                      itemCount: controller.articles.length,
+                                      itemBuilder: (context, index) {
+                                        return HeroNewsElement(
+                                          imageUrl: controller
+                                              .articles[index].urlToImage
+                                              .toString(),
+                                          title: controller.articles[index].title
+                                              .toString(),
+                                          constraints: constraints,
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        }),
+                    ),
+              ));
         });
   }
 }
